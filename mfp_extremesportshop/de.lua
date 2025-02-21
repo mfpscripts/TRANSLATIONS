da9e43b9efb96a2035e4a612db13db525c989750
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-02-21 10:41:55 +0100

    Add files via upload

---
+++ mfp_extremesportshop/de.lua
@@ -0,0 +1,24 @@
+Translation = {
+    ['de'] = {
+        ['open_menu'] = 'Drücke ~o~E~s~ um mit dem Händler zu reden',
+		['weapon_menu_item_label'] = '~g~Kaufen ~s~oder ~r~Verkaufen~s~ Sie diese Ware.',
+		--- HAUPTMENU
+		['dealer'] = 'Händler',
+		['dealer_subtitle'] = 'Dealer des Vertrauens',
+		['parachute'] = 'Fallschirm',
+		['parachute_subtitle'] = 'Kaufen oder Verkaufen Sie ~o~Drogen~s~.',
+	    ['divingmask'] = 'Tauchermaske',
+	    ['divingmask_subtitle'] = 'Kaufen oder Verkaufen Sie ~o~Schießeisen~s~.',
+		--------------------------------------
+		['buy'] = 'Kaufen',
+		['sell'] = 'Verkaufen',
+		['money_in_cash'] = '~g~Bargeld',
+		----------------------------------------
+		['eqiuped_divingmask'] = 'Tauchermaske wurde aufgesetzt und passt wie angegossen!',
+		['already_has_it_fail'] = 'Du hast bereits einen ~b~%s~s~ bei mir gekauft!',
+		['sell_true'] = 'Du verkaufst ~b~%s~s~ an mich wieder für ~o~$%s,00~s~',
+		['buy_true'] = 'Du kaufst ~b~%s~s~ für ~g~$%s,00~s~',
+		['sell_false'] = '~r~Du kannst keine/-n %s verkaufen, wenn du keine hast!',
+		['buy_false'] = 'Du kannst dir dies ~r~nicht~s~ leisten. Es werden ~r~$%s,00~s~ benötigt!',
+    }
+}
