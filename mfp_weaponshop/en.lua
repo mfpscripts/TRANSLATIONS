b3410cfaec38146b5f2fc10c463e74ea952755bd
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:51:01 +0100

    Create en.lua

---
+++ mfp_weaponshop/en.lua
@@ -0,0 +1,23 @@
+['en'] = {
+        ['open_menu'] = 'Press ~INPUT_PICKUP~ to talk to the seller',
+		--- Main
+		--------------------------------------
+		['buy_ad'] = 'Buy this ~o~unique weapon~w~ at a special price',
+		['money_in_cash'] = '~g~Cash',
+		['weaponshop'] = 'Ammunation',
+		['weapon_sell'] = 'Sale of weapons',
+		['license'] = 'License',
+		['license_bought'] = 'Here is ~g~your weapon license~w~. ~Be careful with it, otherwise it will quickly be gone again...',
+		['license_first'] = 'You must buy a ~o~License~w~ before you can buy a weapon.',
+		['fail_got_same_weapon'] = 'You already have one ~o~%s~w~ and do not need another.',
+		['fail_money1'] = 'You do not have ~r~%s$~w~ for the weapon.';
+		['bought_weapon_succes'] = 'You bought a ~o~%s~w~ for ~g~%s$~w~. Do not do junk with it!',
+		['buy_license'] = 'Purchase license',
+		['cancel'] = '~r~Cancel',
+		--------------
+		['pistol'] = 'Pistol',
+		['pistol50'] = '50er Pistol',
+		['snspistol'] = 'SNS Pistol',
+		['pumpgun'] = 'Pumpgun',
+		----------------------------------------
+    },
