dc9dd5392433233151a114915884f30dcee143d8
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:46:37 +0100

    Create de.lua

---
+++ mfp_illegaldealer/de.lua
@@ -0,0 +1,43 @@
+['de'] = {
+        ['open_menu'] = 'Drücke ~INPUT_CONTEXT~ um mit dem Dealer zu reden',
+		['weapon_menu_item_label'] = '~g~Kaufen ~s~oder ~r~Verkaufen~s~ Sie diese Ware.',
+		--- HAUPTMENU
+		['dealer'] = 'Händler',
+		['dealer_subtitle'] = 'Dealer des Vertrauens',
+		['drugs'] = 'Drogen',
+		['drugs_subtitle'] = 'Kaufen oder Verkaufen Sie ~o~Drogen~s~.',
+	    ['weapons'] = 'Waffen',
+	    ['weapons_subtitle'] = 'Kaufen oder Verkaufen Sie ~o~Schießeisen~s~.',
+	    --- WAFFEN
+		['pistole'] = 'Pistole',
+		['microsmg'] = 'MikroSMG',
+		['pumpgun'] = 'Pumpgun',
+		['assaultrifle'] = 'Assaultrifle',
+	    ['specialcarabiner'] = 'Spezialkarabiner',
+	    ['sniper'] = 'Snipergewehr',
+		['appistol'] = 'Appistole',
+		['carabinefile'] = 'Karabinergewehr',
+		--- DROGEN
+		['cocain'] = 'Kokain',
+		['heroin'] = 'Heroin',
+		['meth'] = 'Meth',
+		['lsd'] = 'LSD',
+	    ['marijuana'] = 'Marijuana',
+		--------------------------------------
+		['buy'] = 'Kaufen',
+		['sell'] = 'Verkaufen',
+		----------------------------------------
+		['notification_drugs_part_a'] = 'Hier ist deine ~g~',
+		['notification_drugs_part_b'] = '~s~. Sei vorsichtig damit!',
+		['not_enought_money_drugs'] = 'Sie haben ~r~nicht~s~ genug Bargeld, kommen Sie mit mehr zurück, wenn Sie etwas kaufen wollen!',
+		['sell_drugs_true'] = 'Danke, für den Verkauf von ~o~%s ~s~im Wert von ~g~%s$ ~s~an mich!',
+		['sell_drugs_failed'] = 'Willst du mich ~r~verarschen~s~? Du brauchst auch ~o~%s ~s~um es mir zu verkaufen.',
+		------------- WEAPON NOTIFICATION -----------
+	    ['already_has_weapon_fail'] = 'Du hast bereits ein/-e ~o~%s~s~ bei dir, bitte komm ~r~ohne~s~ zurück, wenn du noch ein/-e willst!',
+	    ['weapon_buy_passed'] = 'Hier ist dein/-e ~o~%s ~s~die du für ~g~%s$~s~ bekommst! Wehe es fällt auf mich zurück!',
+		['weapon_buy_not_enought'] = 'Du hast keine ~o~%s$ dabei. ~r~Keine~s~ Moneten - ~r~Keine ~s~Schieseisen!',
+		-- V1.1
+		['blackmoney'] = '~r~Schwarzgeld',
+		['money'] = '~g~Bargeld',
+
+    },
