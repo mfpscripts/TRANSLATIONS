1a9b57a513f0f3921d3c601bac97a321a527ee80
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:54:15 +0100

    Create de.lua

---
+++ mfp_jobduty/de.lua
@@ -0,0 +1,11 @@
+['de'] = {
+  ['open_menu'] = 'Drücke ~INPUT_CONTEXT~ um deinen Dienststatus zu verwalten',
+  ['status'] = 'Dienststatus',
+  ['status_disc'] = 'Gehe in/aus den Dienst',
+  ['duty'] = 'Statusverwaltung',
+  ['duty_disc'] = 'Gehe in & aus den Dienst',
+  ['offduty_msg'] = '~p~Du befindest dich nun außerdienst, schönen Feierabend!.',
+  ['onduty_msg_01'] = '~p~Du befindest dich nun im Dienst.',
+  ['onduty_msg_02'] = '~p~Du befindest dich nun im Polizeidienst.',
+  ['duty_failed'] = '~r~Du bist nicht dazu berechtigt den Dienststatus zu wechseln.',
+},
