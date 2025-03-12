c7e8a657ebad6fad498aca1bb43b60999d91c1b0
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:44:58 +0100

    Create fr.lua

---
+++ mfp_scrapyard/fr.lua
@@ -0,0 +1,13 @@
+['fr'] = { -- french
+       ['open_menu'] = 'Appuie sur ~INPUT_PICKUP~ pour parler à ouvrier',
+       ['seller'] = 'Vendeur',
+       ['sell_but'] = 'Vendre de la ferraille',
+       ['sell_info'] = 'Vends tous les ~o~déchets~s~ que tu as ramassés ! Les prix peuvent se dénaturer',
+       ['sell_worked_not'] = 'Tu me as vendu ~o~de la camelote~s~ pour une valeur de ~g~%s~s~ $.',
+       ['sell_failed_not'] = 'Tu ne as pas de ~s~déchets sur toi que tu pourrais transformer en argent !',
+       ['scrapyard'] = 'Cour de ferraille',
+       ['search'] = 'Appuie sur E pour ramasser les débris',
+       ['search_scrap'] = 'Collecte de ferraille',
+       ['search_scrap_not'] = 'Tu as trouvé ~o~de la camelote~, va vite me la vendre !', 
+
+   },
