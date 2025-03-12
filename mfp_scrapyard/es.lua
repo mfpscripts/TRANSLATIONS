0f7718c6573302717d321841eedcc7c2b0998b5d
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:45:22 +0100

    Create es.lua

---
+++ mfp_scrapyard/es.lua
@@ -0,0 +1,12 @@
+['es'] = { -- spanish
+       ['open_menu'] = 'Pulse ~INPUT_PICKUP~ para hablar con el trabajador',
+      ['seller'] = 'Vendedor',
+      ['sell_but'] = 'Vender chatarra',
+      ['sell_info'] = '¡Vende toda la chatarra que recogiste! Los precios pueden desnaturalizarse...',
+      ['sell_worked_not'] = 'Me vendiste ~o~scrap~s~ por valor de ~g~%s~s~ $.',
+      ['sell_failed_not'] = '¡No tienes ninguna chatarra contigo que puedas convertir en dinero!',
+      ['scrapyard'] = 'Chatarrero',
+      ['search'] = 'Pulsa E para recoger la chatarra',
+      ['search_scrap'] = 'Recogida de chatarra',
+      ['search_scrap_not'] = '¡Encontraste ~o~chatarra~s~, ve y véndemela rápido!',
+   },
