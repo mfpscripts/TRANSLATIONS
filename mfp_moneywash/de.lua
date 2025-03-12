db2e4a00e2d0281c601b6ee8214205abaa34e772
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:47:34 +0100

    Create de.lua

---
+++ mfp_moneywash/de.lua
@@ -0,0 +1,17 @@
+['de'] = {
+	    ----------- MENU -----------
+        ['open_menu'] = 'Drücke ~INPUT_PICKUP~ um mit dem Mann zu reden',
+		['dealer'] = 'Geldwäscher',
+		['dealer_contact'] = 'Kontaktmann',
+		['dealer_subtitle'] = 'Wasche dein Geld bei mir!',
+		['firstone'] = '~r~500$',
+	    ['secondone'] = '~r~5.000$',
+		['tripple'] = '~r~10.000$',
+        ['custom_washing_button'] = '~o~Eigene Summe',
+		['money_in_cash'] = '~r~Schwarzgeld',
+		['custom_washing_text'] = 'Wie viel Geld benötigst du gewaschen?',
+		-------- NOTIFICATIONS --------
+		['notification_1'] = 'Es werden ~r~%s$~s~ gewaschen! Bitte warte kurz...',
+		['notification_2'] = 'Deine ~r~%s$~s~ wurden ~g~erfolgreich~s~ gewaschen, verpiss dich jetzt! Der Rest ist mein Anteil!',
+		['notification_3'] = '~r~Du musst auch %s$ haben, um es zu waschen...',
+    },
