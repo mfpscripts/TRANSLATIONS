07605457d21f226ac754491043143a13696ee7f5
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:44:44 +0100

    Create de.lua

---
+++ mfp_scrapyard/de.lua
@@ -0,0 +1,13 @@
+['de'] = { -- german
+       ['open_menu'] = 'Drücke ~INPUT_PICKUP~ um mit dem Arbeiter zu sprechen',
+       ['seller'] = 'Verkäufer',
+       ['sell_but'] = 'Schrott verkaufen',
+       ['sell_info'] = 'Verkaufe sämtlichen ~o~Schrott~s~ den Sie aufgesammelt haben! Die Preise können denaturieren...',
+       ['sell_worked_not'] = 'Du hast ~o~Schrott~s~ bei mir verkauft im Wert von ~g~%s~s~ $',
+       ['sell_failed_not'] = 'Du hast ~r~keinen ~s~Schrott dabei, den du zu Geld machen könntest!',
+       ['scrapyard'] = 'Schrottplatz',
+       ['search'] = 'Drücke E um Schrott aufzuheben',
+       ['search_scrap'] = 'Schrottsammeln',
+       ['search_scrap_not'] = 'Du hast ~o~Schrott~s~ gefunden, gehe und verkaufe es schnell mir!', 
+
+   },
