3e9a953e8c2329ee5f2a15e08507ccb9ff8120df
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:50:47 +0100

    Create de.lua

---
+++ mfp_weaponshop/de.lua
@@ -0,0 +1,23 @@
+['de'] = {
+        ['open_menu'] = 'Drücke ~INPUT_PICKUP~ um mit dem Verkäufer zu reden',
+		--- HAUPTMENU
+		--------------------------------------
+		['buy_ad'] = 'Kaufe diese ~o~einzigartige Waffe~w~ zu einem Sonderpreis',
+		['money_in_cash'] = '~g~Bargeld',
+		['weaponshop'] = 'Ammunation',
+		['weapon_sell'] = 'Waffenverkauf',
+		['license'] = 'Linzens',
+		['license_bought'] = 'Hier ist ~g~deine Waffenlizens~w~. Geh vorsichtig damit um, sonst ist diese schnell wieder weg...',
+		['license_first'] = 'Du musst eine ~o~Lizens kaufen~w~ bevor du eine Waffe kaufen kannst.',
+		['fail_got_same_weapon'] = 'Du hast bereits eine ~o~%s~w~ und braust keine weitere.',
+		['fail_money1'] = 'Du hast keine ~r~%s$~w~ für die Waffe.';
+		['bought_weapon_succes'] = 'Du hast eine ~o~%s~w~ für ~g~%s$~w~ gekauft. Mach bloß keinen Schrott damit!',
+		['buy_license'] = 'Lizens erwerben',
+		['cancel'] = '~r~Abbrechen',
+		--------------
+		['pistol'] = 'Pistole',
+		['pistol50'] = '50er Pistole',
+		['snspistol'] = 'Billigknarre',
+		['pumpgun'] = 'Pumpgun',
+		----------------------------------------
+    },
