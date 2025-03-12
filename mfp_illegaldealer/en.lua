80ecc3ad979e1fdf7931eeabb14ca2cc47143656
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-12 08:46:21 +0100

    Create en.lua

---
+++ mfp_illegaldealer/en.lua
@@ -0,0 +1,43 @@
+['en'] = {
+        ['open_menu'] = 'Press ~INPUT_CONTEXT~ to talk with the dealer',
+		['weapon_menu_item_label'] = '~g~Buy~s~ or ~r~sell ~s~this ware.',
+		--- MAIN MENU
+		['dealer'] = 'Dealer',
+		['dealer_subtitle'] = 'Dealer of your trust',
+		['drugs'] = 'Drugs',
+		['drugs_subtitle'] = 'Buy or sell ~o~Drugs~s~.',
+	    ['weapons'] = 'Weapons',
+	    ['weapons_subtitle'] = 'Buy or sell ~o~Weapons~s~.',
+		--- WEAPONS
+		['pistole'] = 'Pistol',
+		['microsmg'] = 'MikroSMG',
+		['pumpgun'] = 'Pumpgun',
+		['assaultrifle'] = 'Assaultrifle',
+	    ['specialcarabiner'] = 'Specialcarabiner',
+	    ['sniper'] = 'Sniper',
+		['appistol'] = 'Appistol',
+		['carabinefile'] = 'Carabinerifle',
+		--- Drugs
+		['cocain'] = 'Cocain',
+		['heroin'] = 'Heroin',
+		['meth'] = 'Meth',
+		['lsd'] = 'LSD',
+	    ['marijuana'] = 'Marijuana',
+		--------------------
+		['buy'] = 'Buy',
+		['sell'] = 'Sell',
+		--------- DRUGS NOTIFICATION ---------------
+		['notification_drugs_part_a'] = 'Here is your ~g~',
+		['notification_drugs_part_b'] = '~s~. Be careful with it!',
+		['not_enought_money_drugs'] = 'You do ~r~not~s~ have enought Cash, come back with more if you want to buy something!',
+		['sell_drugs_true'] = 'Thanks for selling your ~o~%s ~s~for ~g~%s$ ~s~to me!',
+		['sell_drugs_failed'] = 'Are you ~r~kidding~s~ me? You also need ~o~%s ~s~to offer it to me.',
+		------------- WEAPON NOTIFICATION -----------
+	    ['already_has_weapon_fail'] = 'You already have a ~o~%s~s~ with you, please come back ~r~without~s~ one if you still want one!',
+	    ['weapon_buy_passed'] = 'Here is your ~o~%s ~s~you bought for ~g~%s$~s~! Do ~r~not~s~ let anything fall on me!',
+		['weapon_buy_not_enought'] = 'You do not have ~o~%s$ with you. ~r~No~s~ money - ~r~no ~s~weapon!',
+		-- V1.1
+		['blackmoney'] = '~r~Blackmoney',
+		['money'] = '~g~Cash',
+
+    },
